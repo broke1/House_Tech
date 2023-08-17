24319ffe0ace5f3e3eb044d27548d722145d6d72
broke1 <fredswork@list.ru>
2023-08-17 19:56:30 +0300

    start work with house tech

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template class="app">
+  <Header />
+</template>
+
+<script>
+
+
+import Header from './components/Header/Header.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style src="./App.sass" lang="sass"></style>
+
+
+
