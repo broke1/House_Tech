4e67d10da8301d676093397e54d2be2493f5fc03
broke1 <fredswork@list.ru>
2023-08-21 10:15:58 +0300

    add menu

---
+++ src/components/UniversalPage/Menu/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="menu_section">
+    <div class="menu_section_transition_block"></div>
+    <div class="menu_section_links">
+      <div class="menu_section_links_container">
+        <div v-for="link in menu" :key="link.name" class="menu_section_links_container_link_block">
+          <a :href="link.url" class="menu_section_links_container_link_block_url">{{ link.name }}</a>
+          <div v-if="link.childs" class="menu_section_links_container_link_block_arrow">
+            <div class="menu_section_links_container_link_block_arrow__left"></div>
+            <div class="menu_section_links_container_link_block_arrow__right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MenuSection',
+  props: {
+    menu: Array
+  }
+}
+</script>
+
+<style src="./Menu.sass" lang="sass"></style>
+
+
--- src/components/UniversalPage/UniversalPage.vue
+++ src/components/UniversalPage/UniversalPage.vue
@@ -1,15 +1,64 @@
 <template>
   <Header />
+  <Menu :menu="menu"/>
 </template>
 
 <script>
 
 import Header from './Header/Header.vue'
+import Menu from './Menu/Menu.vue'
 
 export default {
   name: 'UniversalPage',
   components: {
-    Header
+    Header,
+    Menu
+  },
+  data() {
+    return {
+      menu: [
+        {
+          name: "Главная",
+          url: "#"
+        },
+        {
+          name: "Портфолио",
+          url: "#"
+        },
+        {
+          name: "Услуги",
+          url: "#",
+          childs: [
+            {
+              name: "Разработка Брендбука",
+              url: "#",
+            },
+            {
+              name: "Разработка Логотипа",
+              url: "#",
+            }
+          ]
+        },
+        {
+          name: "Подробнее",
+          url: "#",
+          childs: [
+            {
+              name: "О нас",
+              url: "#",
+            },
+            {
+              name: "О компании",
+              url: "#",
+            }
+          ]
+        },
+        {
+          name: "Контакты",
+          url: "#"
+        }
+      ]
+    }
   }
 }
 </script>
