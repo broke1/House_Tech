7f5f3b14cb61c756c729a42b33c07b3d18a59637
broke1 <fredswork@list.ru>
2023-08-22 08:18:17 +0300

    add title section

--- src/components/UniversalPage/Menu/Menu.vue
+++ src/components/UniversalPage/Menu/Menu.vue
@@ -1,9 +1,14 @@
 <template>
   <section class="menu_section">
+    <div class="menu_section_back"></div>
     <div class="menu_section_transition_block"></div>
     <div class="menu_section_links">
       <div class="menu_section_links_container">
-        <div v-for="link in menu" :key="link.name" class="menu_section_links_container_link_block">
+        <div 
+          v-for="link in menu" 
+          :key="link.name" 
+          class="menu_section_links_container_link_block"
+        >
           <a :href="link.url" class="menu_section_links_container_link_block_url">{{ link.name }}</a>
           <div v-if="link.childs" class="menu_section_links_container_link_block_arrow">
             <div class="menu_section_links_container_link_block_arrow__left"></div>
@@ -12,16 +17,34 @@
         </div>
       </div>
     </div>
+    <Title 
+      :title="title"
+      :subtitle="subtitle"
+      :breadcrumbs ="breadcrumbs"
+    />
   </section>
 </template>
 
 <script>
 
+import Title from './Title/Title.vue'
 
 export default {
   name: 'MenuSection',
+  components: {
+    Title
+  },
   props: {
     menu: Array
+  },
+  data() {
+    return {
+      breadcrumbs: ["Главная","Услуги","Разработка брендбука"],
+      title: "Разработка брендбука",
+      subtitle: `Мы специализируемся на создании эффективных и уникальных материалов для вашего бизнеса, 
+      и готовы предложить вам наши профессиональные услуги в области разработки брендбука.
+      `
+    }
   }
 }
 </script>
---
+++ src/components/UniversalPage/Menu/Title/Title.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="title_section">
+    <div class="title_section_container">
+      <div class="title_section_container_left_part">
+        <div class="title_section_container_left_part_breadcrumbs">
+          <div 
+            v-for="(crumb, index) in breadcrumbs" 
+            :key="crumb" 
+            class="title_section_container_left_part_breadcrumbs_crumb"
+            >
+            <div class="title_section_container_left_part_breadcrumbs_crumb_text">
+              {{ crumb }}
+            </div>
+            <div 
+              v-if="index != breadcrumbs.length-1"
+              class="title_section_container_left_part_breadcrumbs_crumb_delimetr"
+            >
+            {{ "/" }}
+            </div>
+          </div>
+        </div>
+        <div class="title_section_container_left_part_title">
+          {{ title }}
+        </div>
+        <div class="title_section_container_left_part_underline"></div>
+        <div class="title_section_container_left_part_subtitle">
+          {{ subtitle }}
+        </div>
+      </div>
+      <div class="title_section_container_right_part"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TitleSection',
+  props: {
+    breadcrumbs: Array,
+    title: String,
+    subtitle: String
+  }
+}
+</script>
+
+<style src="./Title.sass" lang="sass"></style>
+
+
