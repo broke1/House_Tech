3d67ac61176dd12ebfbff72af6950b73292259f3
broke1 <fredswork@list.ru>
2023-08-25 12:38:35 +0300

    almost done left fix btn and line

--- src/components/UniversalPage/Footer/Footer.vue
+++ src/components/UniversalPage/Footer/Footer.vue
@@ -40,15 +40,89 @@
           <div class="footer_section_container_contacts_container_popup_btn_dop_img"></div>
         </div>
       </div>
+      <div class="footer_section_container_links">
+        <Links 
+          v-for="links in this.$store.state.links"
+          :key="links.title"
+          :links="links"
+        />
+      </div>
+      <div class="footer_section_container_labels">
+        <div class="footer_section_container_labels_block footer_section_container_labels_block_first">
+          <div class="footer_section_container_labels_block_line"></div>
+          <div class="footer_section_container_labels_block_text_block">
+            <div class="footer_section_container_labels_block_text_block_text">
+              {{ `Весь контент этого сайта (изображения, видеоролики, тексты и пр.) являются объектами авторских прав ООО "Хаус Тех". 
+              Их полное или частичное распространение, изменение или копирование без предварительного письменного согласия ООО "Хаус Тех" запрещено.` }}
+            </div>
+            <div class="footer_section_container_labels_block_text_block_text">
+              {{  `Любое нарушение авторских и смежных прав преследуется в соответствии с законодательством Российской Федерации, международными актами и 
+              влечет наступление гражданской, административной и уголовной ответственности.` }}
+            </div>
+          </div>
+        </div>
+        <div class="footer_section_container_labels_block footer_section_container_labels_block_second">
+          <div class="footer_section_container_labels_block_icon_ros_patent"></div>
+        </div>
+        <div class="footer_section_container_labels_block footer_section_container_labels_block_third">
+          <div class="footer_section_container_labels_block_icon_min"></div>
+        </div>
+        <div class="footer_section_container_labels_block footer_section_container_labels_block_fourth">
+          <div class="footer_section_container_labels_block_icon_artstation"></div>
+        </div>
+      </div>
+      <div class="footer_section_container_end">
+        <div class="footer_section_container_end_left">
+          <div class="footer_section_container_end_left_logo"></div>
+          <div class="footer_section_container_end_left_text">
+            {{ "2009-2023© House Tech, Все права защищены" }}
+          </div>
+        </div>
+        <div class="footer_section_container_end_right">
+          <div class="footer_section_container_end_right_other_resourses">
+            <div class="footer_section_container_end_right_other_resourses_icon"></div>
+            <div class="footer_section_container_end_right_other_resourses_text_block">
+              <div class="footer_section_container_end_right_other_resourses_text_block_text">
+                {{ "Другие ресурсы:" }}
+              </div>
+              <a 
+                href="https://housetech-it.ru/"
+                class="footer_section_container_end_right_other_resourses_text_block_link">
+                {{ "www.housetech-it.ae" }}
+            </a>
+            </div>
+          </div>
+          <div class="footer_section_container_end_right_links">
+            <div class="footer_section_container_end_right_links_text">
+              {{ "Связаться с нами" }}
+            </div>
+            <div class="footer_section_container_end_right_links_social_btn">
+              <a href="" class="footer_section_container_end_right_links_social_btn_link footer_section_container_end_right_links_social_btn_link__telegram">
+                <img src="../../../assets/social_telegram_header.svg">
+              </a>
+              <a href="" class="footer_section_container_end_right_links_social_btn_link footer_section_container_end_right_links_social_btn_link__skype">
+                <img src="../../../assets/social_skype_header.svg">
+              </a>
+              <a href="" class="footer_section_container_end_right_links_social_btn_link footer_section_container_end_right_links_social_btn_link__whatsapp">
+                <img src="../../../assets/social_whatsapp_header.svg">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 
+import Links from './Links/Links.vue'
 
 export default {
-  name: 'FooterSection'
+  name: 'FooterSection',
+  components: {
+    Links
+  },
 }
 </script>
 
---
+++ src/components/UniversalPage/Footer/Links/Links.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="links">
+    <div class="links_title">
+      {{ links.title }}
+    </div>
+    <a 
+      v-for="item in links.linksItem"
+      :href="item.url"
+      :key="item.name"
+      class="links_item"
+    >
+      <div 
+        class="links_item_icon"
+        :class="{
+          'links_item_icon_doc': item.icon == 'doc',
+          'links_item_icon_eight': item.icon == 'eight'
+        }"
+      ></div>
+      <div class="links_item_text">
+        {{ item.name }}
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'LinksDiv',
+  props: {
+    links: Object
+  }
+}
+</script>
+
+<style src="./Links.sass" lang="sass"></style>
+
+
